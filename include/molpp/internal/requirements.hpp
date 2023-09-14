e596c6708cc43a5acad39c711ce03625ef295817
Caio S. Souza <caiobiounb@gmail.com>
2023-09-14 20:59:53 +0100

    Simplified class hierarchy for AtomAggregates
    This is part of a refactor in preparation for adopting the new property system.

--- include/molpp/internal/requirements.hpp
+++ include/molpp/internal/requirements.hpp
@@ -2,6 +2,7 @@
 #define REQUIREMENTS_HPP
 
 #include <ranges>
+#include <vector>
 #include <concepts>
 
 namespace mol::internal
@@ -12,6 +13,16 @@ concept IndexRange = requires(T t)
 {
     std::ranges::range<T>
     && std::unsigned_integral<typename T::value_type>;
+};
+
+template <class Derived>
+class AtomAggregate;
+
+template <class T>
+concept IsAtomAggregate = requires(T t)
+{
+    std::derived_from<T, AtomAggregate<T>>;
+    {t.atom_indices()} -> std::convertible_to<std::vector<index_t>>;
 };
 
 } // namespace mol::internal
